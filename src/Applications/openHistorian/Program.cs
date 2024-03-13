4a4e328283d00b2dc9108dcffb9e42fa3a4d6110
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2024-03-13 06:13:44 -0500

    Updated project paths

--- src/Applications/openHistorian/Program.cs
+++ src/Applications/openHistorian/Program.cs
@@ -8,7 +8,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        SQLitePCL.Batteries.Init();
+        //SQLitePCL.Batteries.Init();
 
         Settings settings = new();
 
