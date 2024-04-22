e86140bc144d2894affbe8a21f93148d2d0fc2ac
Christoph Lackner <clackner@gridprotectionalliance.org>
2024-04-22 14:48:10 -0400

    Added SchemaDerfinition Project

---
+++ src/Libraries/SchemaDefinition/SchemaDefinition/GemstoneMigrationAttribute.cs
@@ -0,0 +1,20 @@
+﻿
+namespace SchemaDefinition
+{
+    /// <summary>
+    /// Mark all migrations with this INSTEAD of [Migration].
+    /// </summary>
+    public class GemstoneMigrationAttribute : FluentMigrator.MigrationAttribute
+    {
+        public GemstoneMigrationAttribute(int branchNumber, int year, int month, int day, string author)
+           : base(CalculateValue(branchNumber, year, month, day))
+        {
+            this.Author = author;
+        }
+        public string Author { get; private set; }
+        private static long CalculateValue(int branchNumber, int year, int month, int day)
+        {
+            return branchNumber * 1000000000000L + year * 100000000L + month * 1000000L + day * 10000L;
+        }
+    }
+}
