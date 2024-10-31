9f225814af64df08542b4f2bf227f5e7bddca99c
Christoph Lackner <clackner@gridprotectionalliance.org>
2024-10-31 12:18:42 -0400

    Added Type and Assembly endpoints to InputAdapter Controller

--- src/Applications/openHistorian.WebUI/Controllers/InputAdaptersController.cs
+++ src/Applications/openHistorian.WebUI/Controllers/InputAdaptersController.cs
@@ -1,6 +1,8 @@
 ﻿using openHistorian.Model;
 using Gemstone.Web.APIController;
 using Microsoft.AspNetCore.Mvc;
+using Gemstone.Data.Model;
+using Gemstone.Timeseries.Adapters;
 
 namespace openHistorian.WebUI.Controllers;
 
@@ -8,4 +10,40 @@ namespace openHistorian.WebUI.Controllers;
 [ApiController]
 public class InputAdaptersController : ModelController<CustomInputAdapter>
 {
-}
+    /// <summary>
+    /// Gets all Assemblies holding input adapters.
+    /// </summary>
+    /// <returns>An <see cref="IActionResult"/> containing an <see cref="IEnumerable{ValueLabel}"/> of the assemblies.</returns>
+    [HttpGet, Route("AssemblyNames")]
+    public async Task<IActionResult> GetAssemblies(CancellationToken cancellationToken)
+    {
+        if (!GetAuthCheck())
+            return Unauthorized();
+
+        return await Task<IActionResult>.Run(() =>
+            Ok(AdapterCollectionHelper<IInputAdapter>.GetAdapters().Select(adapter => new ValueLabel()
+            {
+                Value = adapter.Assembly,
+                Label = adapter.Assembly
+            }).DistinctBy((v) => v.Value).OrderBy(adapter => adapter.Label)));    
+    }
+
+    /// <summary>
+    /// Gets all input Adapters from a specified Assembly.
+    /// </summary>
+    /// <returns>An <see cref="IActionResult"/> containing an <see cref="IEnumerable{ValueLabel}"/> of the Types.</returns>
+    [HttpGet, Route("Types/{assemblyName}")]
+    public async Task<IActionResult> GetTypes(string assemblyName, CancellationToken cancellationToken)
+    {
+        if (!GetAuthCheck())
+            return Unauthorized();
+
+        return await Task<IActionResult>.Run(() =>
+            Ok(AdapterCollectionHelper<IInputAdapter>.GetAdapters().Where((a) => String.Compare(a.Assembly,assemblyName,true) == 0)
+            .Select(adapter => new ValueLabel()
+            {
+                Value = adapter.Type,
+                Label = adapter.Header
+            }).DistinctBy((v) => v.Value).OrderBy(adapter => adapter.Label)));
+    }
+} 
