7448fcfde70c8007d61e9e01ea9232da866f0875
prestoncraw <prestoncrawford530@gmail.com>
2024-11-20 16:59:23 -0500

    Remove fallback

--- src/Applications/openHistorian.WebUI/WebServer.cs
+++ src/Applications/openHistorian.WebUI/WebServer.cs
@@ -182,7 +182,6 @@ public class WebServer(WebServerConfiguration configuration)
         {
             endpoints.MapRazorPages();
             endpoints.MapControllers();
-            endpoints.MapFallbackToPage("/Index");
         });
 
     }
