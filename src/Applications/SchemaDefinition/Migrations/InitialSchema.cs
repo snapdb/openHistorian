b96e8d7ddf3e509a43509499009e0eb3029927df
Christoph Lackner <52460212+clackner-gpa@users.noreply.github.com>
2025-02-23 14:20:49 -0500

    Added Alarm and eventDetails

--- src/Applications/SchemaDefinition/Migrations/InitialSchema.cs
+++ src/Applications/SchemaDefinition/Migrations/InitialSchema.cs
@@ -88,6 +88,9 @@ public class InitialSchema : Migration
         Delete.Table("AlarmDevice");
         Delete.Table("DataAvailability");
 
+        // Event Related
+        Delete.Table("Alarm");
+        Delete.Table("EventDetails");
         // Views
         Execute.DeleteView("RuntimeOutputStreamMeasurement");
         Execute.DeleteView("RuntimeHistorian");
@@ -514,6 +517,32 @@ public class InitialSchema : Migration
             .WithColumn("GoodAvailableData").AsDouble().NotNullable()
             .WithColumn("BadAvailableData").AsDouble().NotNullable()
             .WithColumn("TotalAvailableData").AsDouble().NotNullable();
+
+        // EventRelated
+        Create.Table("Alarm")
+            .WithColumn("ID").AsInt32().PrimaryKey().Identity()
+            .WithColumn("TagName").AsString(200).NotNullable()
+            .WithColumn("SignalID").AsString(36).NotNullable().ForeignKey("Measurement", "SignalID")
+            .WithColumn("InputMeasurementKeys").AsString(int.MaxValue).NotNullable()
+            .WithColumn("Description").AsString(int.MaxValue).Nullable()
+            .WithColumn("Severity").AsInt32().NotNullable()
+            .WithColumn("Operation").AsInt32().NotNullable()
+            .WithColumn("Combination").AsInt32().NotNullable()
+            .WithColumn("SetPoint").AsDouble().Nullable();
+            .WithColumn("Tolerance").AsDouble().Nullable();
+            .WithColumn("Delay").AsDouble().Nullable();
+            .WithColumn("Hysteresis").AsDouble().Nullable();
+            .WithColumn("Timeout").AsDouble().Nullable();
+            .WithColumn("Enabled").AsBoolean().NotNullable().WithDefaultValue(false);
+
+        Create.Table("EventDetails")
+            .WithColumn("ID").AsInt32().PrimaryKey().Identity()
+            .WithColumn("EventGuid").AsString(36).NotNullable()
+            .WithColumn("StartTime").AsDateTime().NotNullable()
+            .WithColumn("EndTime").AsDateTime().Nullable()
+            .WithColumn("MeasurementID").AsString(36).NotNullable().ForeignKey("Measurement", "SignalID")
+            .WithColumn("Details").AsString(int.MaxValue).NotNullable()
+            .WIthColumn("Type").AsString(20).NotNullable();
 
         Execute.AddRunTimeSync("Historian");
         Execute.AddRunTimeSync("OutputStream");
