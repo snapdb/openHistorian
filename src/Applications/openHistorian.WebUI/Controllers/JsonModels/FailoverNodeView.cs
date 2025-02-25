1d9afb1863dceb969109e3c0bf12dcf3e3d1c013
Christoph Lackner <clackner@gridprotectionalliance.org>
2025-02-25 16:52:39 -0500

    Added FailoverController for UI

---
+++ src/Applications/openHistorian.WebUI/Controllers/JsonModels/FailoverNodeView.cs
@@ -0,0 +1,13 @@
+﻿using Gemstone.Data.Model;
+using Gemstone.Expressions.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace openHistorian.WebUI.Controllers.JsonModels;
+
+public class FailoverNodeView
+{
+    [PrimaryKey(true)]
+    public string SystemName { get; set; }
+    public int Priority { get; set; }
+    public DateTime LastLog { get; set; }
+}
