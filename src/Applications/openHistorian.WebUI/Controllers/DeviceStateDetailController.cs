91a46bba8873de7a0db4dce9fea665e8b23fbb77
prestoncraw <prestoncrawford530@gmail.com>
2025-02-28 14:30:08 -0500

    Added DeviceStateDetailController

---
+++ src/Applications/openHistorian.WebUI/Controllers/DeviceStateDetailController.cs
@@ -0,0 +1,13 @@
+﻿using Gemstone.Web.APIController;
+using GrafanaAdapters.Model.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace openHistorian.WebUI.Controllers;
+
+//rename models and routes to drop alarm
+
+[Route("api/DeviceStateView")]
+[ApiController]
+public class DeviceStateDetailController : ReadOnlyModelController<DeviceStateView>
+{
+}
