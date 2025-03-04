2f1f2a3b75a7125fdfecc42be2f105a921f765b6
prestoncraw <prestoncrawford530@gmail.com>
2025-03-04 13:23:14 -0500

    Add Alarm controller

---
+++ src/Applications/openHistorian.WebUI/Controllers/AlarmController.cs
@@ -0,0 +1,10 @@
+﻿using Gemstone.Web.APIController;
+using Microsoft.AspNetCore.Mvc;
+using Gemstone.Timeseries;
+namespace openHistorian.WebUI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AlarmController : ModelController<Alarm>
+{
+}
