d2fa49e952aed0668a5d71c4697928f16e125690
prestoncraw <prestoncrawford530@gmail.com>
2025-05-06 15:24:12 -0400

    add logic to get proper pointtag and set historianID

--- src/Applications/openHistorian.WebUI/Controllers/AlarmController.cs
+++ src/Applications/openHistorian.WebUI/Controllers/AlarmController.cs
@@ -3,12 +3,17 @@ using Microsoft.AspNetCore.Mvc;
 using Gemstone.Timeseries;
 using Gemstone.Data.Model;
 using Gemstone.Data;
+using Gemstone.EnumExtensions;
+using System.Text;
+using System.Text.RegularExpressions;
+using ServiceInterface;
 namespace openHistorian.WebUI.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public class AlarmController : ModelController<Alarm>
 {
+    private readonly IServiceCommands m_serviceCommands = WebServer.ServiceCommands;
 
     /// <summary>
     /// Creates new record in associated table.
@@ -28,6 +33,9 @@ public class AlarmController : ModelController<Alarm>
         TableOperations<Gemstone.Timeseries.Model.SignalType> signalTypeTableOperations = new(connection);
         Gemstone.Timeseries.Model.SignalType? signalType = signalTypeTableOperations.QueryRecordWhere("Acronym = {0}", "ALRM");
 
+        TableOperations<Model.Historian> historianTableOperations = new(connection);
+        Model.Historian? statHistorian = historianTableOperations.QueryRecordWhere("Acronym = {0}", "STAT");
+
         if (cancellationToken.IsCancellationRequested)
             return Ok();
 
@@ -35,12 +43,13 @@ public class AlarmController : ModelController<Alarm>
         {
             TableOperations<Gemstone.Timeseries.Model.Measurement> measurementTableOperations = new(connection);
             newMeasurement = measurementTableOperations.NewRecord()!;
-            newMeasurement.PointTag = alarmRecord.TagName;
+            newMeasurement.PointTag = GetCleanPointTag(alarmRecord);
             newMeasurement.SignalTypeID = signalType.ID;
+            newMeasurement.HistorianID = statHistorian?.ID;
             measurementTableOperations.AddNewRecord(newMeasurement);
         }
 
-        if(newMeasurement is not null)
+        if (newMeasurement is not null)
         {
             alarmRecord.SignalID = newMeasurement.SignalID;
         }
@@ -48,7 +57,49 @@ public class AlarmController : ModelController<Alarm>
         TableOperations<Alarm> tableOperations = new(connection);
         tableOperations.AddNewRecord(alarmRecord);
         Alarm? foundRecord = tableOperations.QueryRecord(tableOperations.GetNonPrimaryFieldRecordRestriction(alarmRecord));
-
+        m_serviceCommands.ReloadConfig();
         return Ok(foundRecord ?? alarmRecord);
+    }
+
+    /// <summary>
+    /// Gets a clean point tag.
+    /// </summary>
+    /// <param name="pointTag">Point tag.</param>
+    /// <returns>Clean point tag.</returns>
+    private static string GetCleanPointTag(Alarm alarmRecord)
+    {
+        StringBuilder pointTag = new("AL-");
+        switch (alarmRecord.Severity)
+        {
+            case AlarmSeverity.None:
+                pointTag.Append("EXEMPT");
+                break;
+            case AlarmSeverity.Unreasonable:
+                switch (alarmRecord.Operation)
+                {
+                    case AlarmOperation.GreaterThan:
+                    case AlarmOperation.GreaterOrEqual:
+                        pointTag.Append("HIGH");
+                        break;
+                    case AlarmOperation.LessThan:
+                    case AlarmOperation.LessOrEqual:
+                        pointTag.Append("LOW");
+                        break;
+                    default:
+                        pointTag.Append(alarmRecord.Severity.GetDescription());
+                        break;
+                }
+                break;
+            default:
+                pointTag.Append(alarmRecord.Severity.GetDescription());
+                break;
+        }
+
+        pointTag.Append(":");
+
+        // Remove any invalid characters from point tag
+        pointTag.Append(Regex.Replace(alarmRecord.TagName.ToUpperInvariant(), @"[^A-Z0-9\-\+!\:_\.@#\$]", "", RegexOptions.IgnoreCase | RegexOptions.Compiled));
+
+        return pointTag.ToString();
     }
 }
