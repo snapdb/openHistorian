cf981f8e4e7ae8eb7791a4418c57ede9ea2adc92
prestoncraw <prestoncrawford530@gmail.com>
2025-05-07 15:16:19 -0400

    add runtime controllers

---
+++ src/Applications/openHistorian.WebUI/Controllers/RuntimeAdapterControllers.cs
@@ -0,0 +1,24 @@
+﻿using Gemstone.Timeseries.Adapters;
+using Gemstone.Timeseries.Model;
+using Gemstone.Web.APIController;
+using Microsoft.AspNetCore.Mvc;
+using openHistorian.Adapters.Model;
+using openHistorian.Model;
+
+namespace openHistorian.WebUI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RuntimeCustomActionAdapterController : ReadOnlyModelController<IaonActionAdapter>;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RuntimeCustomInputAdapterController : ReadOnlyModelController<IaonInputAdapter>;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RuntimeCustomOutputAdapterController : ReadOnlyModelController<IaonOutputAdapter>;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RuntimeCustomFilterAdapterController : ReadOnlyModelController<IaonFilterAdapter>;
---
+++ src/Libraries/Adapters/openHistorian.Adapters/Model/IaonFilterAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Gemstone.Data.Model;
+using openHistorian.Model;
+
+namespace openHistorian.Adapters.Model;
+public class IaonFilterAdapter : IIaonAdapter
+{
+    [PrimaryKey(true)]
+    public int ID { get; set; }
+
+    [Required]
+    [StringLength(200)]
+    public string AdapterName { get; set; }
+
+    [Required]
+    public string AssemblyName { get; set; }
+
+    [Required]
+    [StringLength(200)]
+    public string TypeName { get; set; }
+
+    public string ConnectionString { get; set; }
+}
