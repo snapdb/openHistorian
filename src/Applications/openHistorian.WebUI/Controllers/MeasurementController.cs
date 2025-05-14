ac4b0906e39c62423b26bc2532ba49f3d179dda1
prestoncraw <prestoncrawford530@gmail.com>
2025-05-14 12:32:18 -0400

    addd override to set manual flag to true

--- src/Applications/openHistorian.WebUI/Controllers/MeasurementController.cs
+++ src/Applications/openHistorian.WebUI/Controllers/MeasurementController.cs
@@ -1,4 +1,6 @@
-﻿using Gemstone.Timeseries.Model;
+﻿using Gemstone.Data.Model;
+using Gemstone.Data;
+using Gemstone.Timeseries.Model;
 using Gemstone.Web.APIController;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,4 +10,23 @@ namespace openHistorian.WebUI.Controllers;
 [ApiController]
 public class MeasurementController: ModelController<Measurement>
 {
+
+    /// <summary>
+    /// Gets a new record.
+    /// </summary>
+    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+    /// <returns>A <see cref="T"/> object or <see cref="Exception"/>.</returns>
+    [HttpGet, Route("New")]
+    public override async Task<IActionResult> New(CancellationToken cancellationToken)
+    {
+        if (!GetAuthCheck())
+            return Unauthorized();
+
+        await using AdoDataConnection connection = CreateConnection();
+        TableOperations<Measurement> tableOperations = new(connection);
+
+        Measurement? result = tableOperations.NewRecord();
+        result.Manual = true;
+        return Ok(result);
+    }
 }
